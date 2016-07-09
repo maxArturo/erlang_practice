0db08021836cfc9555880c1be8ee57d9bbbc6d6f
Max Alcala <malcala@shoppertrak.com>
2016-07-09 11:41:52 -0500

    finish recursive section of exercises

--- exercises/exercises.erl
+++ exercises/exercises.erl
@@ -1,10 +1,18 @@
+% These are answers to the set of exercises found in the official Erlang website:
+% http://erlang.org/course/exercises.html
+
 -module(exercises).
 -export([
          f2c/1,
          c2f/1,
-         convert/1  
+         convert/1,
+         min/1,
+         max/1,
+         min_max/1
         ]).
-
+%%
+%% Simple sequential programs
+%%
 % Write functions temp:f2c(F) and temp:c2f(C) which convert between centigrade and Fahrenheit scales. (hint 5(F-32) = 9C)
 f2c(Fahr) ->
   5 * (Fahr - 32) / 9.
@@ -24,4 +32,60 @@ convert({c, Celsius}) ->
   {f, (Celsius * 9 / 5) + 32};
 convert({f, Fahr}) -> 
   {c, 5 * (Fahr - 32) / 9}.
+
+%%
+%% Simple recursive programs
+%%
+% 1. Write a function lists1:min(L) which returns the mini- mum element of the list L.
+
+min([]) -> 
+  0;
+min([H|T]) -> 
+  min_helper(T, H).
+    
+min_helper([H|T], Min) ->
+  case H < Min of
+      true -> min_helper(T, H);
+      false -> min_helper(T, Min)
+  end;
+
+min_helper([], Min) ->
+  Min.
+
+
+% 2. Write a function lists1:max(L) which returns the maximum element of the list L.
+max([]) -> 
+  0;
+max([H|T]) -> 
+  max_helper(T, H).
+    
+max_helper([H|T], Max) ->
+  case H > Max of
+      true -> max_helper(T, H);
+      false -> max_helper(T, Max)
+  end;
+
+max_helper([], Max) ->
+  Max.
+ 
+% 3. Write a function lists1:min_max(L) which returns a tuple containing the min and max of the list L.
+%
+
+min_max([]) -> 
+  {0, 0};
+min_max([H|T]) -> 
+  min_max_helper(T, H, H).
+    
+min_max_helper([H|T], Min, Max) ->
+  if
+      H > Max ->
+        min_max_helper(T, Min, H);
+      H < Min ->
+        min_max_helper(T, H, Max);
+      true -> 
+        min_max_helper(T, Min, Max)
+  end;
+
+min_max_helper([], Min, Max) ->
+  {Min, Max}.
 
